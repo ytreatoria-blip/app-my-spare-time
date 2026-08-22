fe08c3588e8afb65c4c0e4ce77a5d445c1b20160
ytreatoria-blip <ytreatoria@gmail.com>
2026-08-23 00:28:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/api/health", (req, res) => {
   async function findPlaces(latitude, longitude, radius, query) {
   const endpoints = [
     "https://overpass-api.de/api/interpreter",
-    "https://overpass.kumi.systems/api/interpreter"
+    "https://overpass.private.coffee/api/interpreter"
   ];
 
   for (const endpoint of endpoints) {
